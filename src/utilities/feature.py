ea1a86307a1485d9852b2b7f7564b7537757e769
zahraav <55157358+zahraav@users.noreply.github.com>
2020-11-13 19:37:15 -0800

    done except count and P/C/R

--- src/utilities/feature.py
+++ src/utilities/feature.py
@@ -1,15 +1,40 @@
+class Info:
+    def __init__(self, line): #technology, protocol, country, region, date):
+        self.technology = self.set_technology(line)
+        self.protocol = self.set_protocol(line)
+        #self.country = country
+        #self.region = region
+        self.date = self.set_date(line)
+
+    def set_technology(self,line):
+        if 'Nanopore' in line:
+            return 'N'
+        else:
+            return 'I'
+
+    def set_protocol(self,line):
+        return 'protocol!'
+
+    def set_date(self, line):
+        year_and_Month = line.rsplit('|')[2].rsplit('-')
+        return year_and_Month[0] + '-' + year_and_Month[1]
+
+
+    def _print(self):
+        return str(self.technology) + '  ' + str(self.date)
+    #    return str(self.technology) + '  ' + str(self.protocol) + '  ' + str(self.country) + '  ' + str(
+    #        self.region)
+
+
 class Feature:
-    def __init__(self, groups, count, context, position, info):
+    def __init__(self, groups, count, context, position, info_dictionary):
         self.groups = groups
         self.count = count
         self.context = context
         self.position = position  # starts from 1
-        self.info = info
+        # TODO change information , data structure!
+        self.infoDictionary = info_dictionary
 
-    # def set_features(self, line):
-    #    for i in len(line):
-    #        print(i)
-
-    def print_features(self):
-        return str(self.groups) + '  ' + str(self.count) + '  ' + str(self.context) + '  ' + str(
-            self.position) + '  ' + str(self.info) + '\n'
+    def _print(self):
+        return 'info:\n ' + str(self.groups) + '  ' + str(self.count) + '  ' + str(self.context) + '  ' + str(
+            self.position) + '  ' + '\n' #str(self.infoDictionary.print_info) + '\n'
--- src/utilities/featureExtraction.py
+++ src/utilities/featureExtraction.py
@@ -1,6 +1,8 @@
 from feature import Feature
+from feature import Info
 
 featuresDictionary = {}
+infoDictionary={}
 dictionary_address = 'files/featuresDict'
 
 
@@ -13,7 +15,7 @@ def setcontext(seq, dictionary_counter):
     while i < len(seq) - 2:
         f = Feature(seq[i], 0, seq[i - 2:i + 3:1], i + 1, "info")
         featuresDictionary[dictionary_counter] = f
-        dictionary_counter+=1
+        dictionary_counter += 1
         i += 1
     return dictionary_counter
 
@@ -21,18 +23,16 @@ def setcontext(seq, dictionary_counter):
 def saveData(output_address, data):
     """ This function write the data in the output_address file"""
     with open(output_address, "a") as output_handle:
-        print(data)
         output_handle.write(data)
 
 
-def savedict():
+def savedict(in_dictionary):
     """This fuction pass elements of dictionary for saving to the saveData function"""
-    for elem in featuresDictionary:
-        saveData(dictionary_address, featuresDictionary[elem].print_features())
+    for elem in in_dictionary:
+        saveData(dictionary_address, in_dictionary[elem]._print())
 
 
 def readDate(fasta_address):
-    featuresDictionary.clear()
     """ This function reads modified Fasta file
     input:    fastaAddress address of fasta file
     output:   return data on the file line by line """
@@ -42,11 +42,15 @@ def readDate(fasta_address):
         for line in infile:
             # TODO Save info for every groups
             if '>' in line:
-                saveData(dictionary_address, line)
+                i=Info(line)
+                infoDictionary[dictionary_counter]=i
+
+                print(i._print())
+                #saveData(dictionary_address, line)
+                #savedict(infoDictionary)
             else:
                 dictionary_counter = setcontext(line, dictionary_counter)
 
-    savedict()
-
+    savedict(featuresDictionary)
 
 readDate('files/test.fasta')

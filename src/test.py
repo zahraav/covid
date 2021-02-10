95ee03fec15971bfcf113945de16eda9ee2f04c5
zahraav <55157358+zahraav@users.noreply.github.com>
2021-02-10 00:10:36 -0800

    remove test.py

--- src/test.py
+++
@@ -1,8 +0,0 @@
-
-
-line=">hCoV-19/Canada/MB-NML-610/2020|EPI_ISL_632914|2020-03-27|NorthAmerica|Nanopore"
-print(line.split('|')[0].split('/')[1].lower())
-if(line.split('|')[0].split('/')[1].lower()):
-    print(line)
-else:
-    print('no')

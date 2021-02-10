94aaf810591afa19c503d004cfa0766f005e302d
Zahra Vafadoost <zahra.vafadoost@gmail.com>
2021-02-10 04:43:40 -0330

    DB Linux

--- src/utilities/DBConnection.py
+++ src/utilities/DBConnection.py
@@ -1,17 +1,21 @@
 #import mysql.connector
-import MySQLdb
-"""
+import mysql.connector
+
+#import MySQLdb
+
 mydb = mysql.connector.connect(
     host="localhost",
     user="root",
     password="",
-    database="world2"
-)"""
+    database="world"
+)
+'''
 mydb = MySQLdb.connect(
         host="localhost",
         user="root",
         password="",
         db="world")    
+'''
 
 def query_execute(query):
     #print('query:',query)

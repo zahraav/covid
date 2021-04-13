e8d4a565b165a8abde625e2c0693eb36ededbc97
zahraav <55157358+zahraav@users.noreply.github.com>
2021-04-12 22:32:39 -0700

    update saving data in csv

--- src/parseMSA.py
+++ src/parseMSA.py
@@ -1,22 +1,25 @@
-import csv
-
 from utilities.ReadAndWrite import saveToCsv
 
 
-def saveCSV(fileName, csvList, isHeader):
-    fieldNames = ['id', 'location', 'letter']
-    saveToCsv(fileName, csvList, fieldNames, isHeader)
+def saveCSV(fileName, csvList, isFirstTimeUsingHeader):
+    fieldNames = ['id', 'date', 'location', 'technology', 'index', 'letter']
+    saveToCsv(fileName, csvList, fieldNames, isFirstTimeUsingHeader)
 
 
 fastaFile = "files/test_2.fasta"
-isHeader = True
+firstTimeUsingHeader = True
 nucleotideList = ['A', 'C', 'G', 'T', '.', '-']
 with open(fastaFile) as infile:
     for line in infile:
         line = line.rstrip()
         if line.__contains__('>'):
             header = line
-            id = header.split(r'|')[1]
+            headerSplit = header.split(r'|')
+            seqId = headerSplit[1]
+            date = headerSplit[2]
+            location = headerSplit[3]
+            technology = headerSplit[4]
+
         else:
             newLetterIndex = 0
             letterIndicesForFile = ' '
@@ -24,29 +27,20 @@ with open(fastaFile) as infile:
                 if x not in nucleotideList:
                     letterIndicesForFile = letterIndicesForFile + ' ' + str(newLetterIndex)
 
-                    saveCSV(fastaFile.replace('.fasta','_withExtraLetter.csv'),[id, newLetterIndex, x],isHeader)
-                    isHeader = False
+                    saveCSV(fastaFile.replace('.fasta', '_withExtraLetter.csv'),
+                            [seqId, date, location, technology, newLetterIndex, x], firstTimeUsingHeader)
+                    firstTimeUsingHeader = False
 
                 newLetterIndex += 1
 
-            if letterIndicesForFile is not ' ':
+            if letterIndicesForFile != ' ':
                 letterIndicesForFile = letterIndicesForFile.lstrip()
                 with open(fastaFile.replace(".fasta", "_withExtraLetter.fasta"), 'a', encoding='utf-8') as f1:
                     f1.write(str(letterIndicesForFile))
                     f1.write(' | ')
-                    f1.write(id)
+                    f1.write(seqId)
                     f1.write(' ')
                     f1.write(header)
                     f1.write('\n')
                     f1.write(line)
                     f1.write('\n')
-                print(letterIndicesForFile, ': ', id, ' - ', header, '\n', line)
-
-
-with open('employee_file2.csv', mode='w') as csv_file:
-    fieldnames = ['emp_name', 'dept', 'birth_month']
-    writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-
-    writer.writeheader()
-
-# یه فایل اکسل اینا رو اضافه کنم هر بار حرف جدید دیدم
